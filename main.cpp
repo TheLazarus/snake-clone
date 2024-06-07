113ec178b84d157472aed8a7170b1de0c5bdc004
Sarthak <ss22072000@gmail.com>
2024-06-07 10:34:41 +0530

    feat: Creating the window

--- main.cpp
+++ main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include "raylib.h"
 
-int main()
-{
+Color green{173, 204, 96, 255};
+Color darkGreen{43, 51, 24, 255};
 
-    InitWindow(800, 600, "Snake Clone");
+int cellSize{30};
+int cellCount{25};
+int main()
+
+{
+    std::cout << "Now starting ... " << "\n";
+    int res = cellSize * cellCount;
+    InitWindow(res, res, "Snake");
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(green);
 
         EndDrawing();
     }
